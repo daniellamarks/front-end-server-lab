912608b0d0f742b06aa3b140f63d4393c92488b7
Daniella Marks <marks.daniella@gmail.com>
2021-05-18 07:58:51 -0700

    hikeform npm run build

--- src/hike-add/HikeAddPage.js
+++ src/hike-add/HikeAddPage.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import HikeForm from '../common/HikeForm';
+import HikeForm from '../common/HikeForm.js';
 // import Loader from '../common/Loader';
 import { addHike } from '../utils/hikes-api';
 import './HikeAddPage.css';
